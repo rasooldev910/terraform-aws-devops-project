fa461295c129bb79bb681d010a7a9fea1fc52d6e
syed rasool <rasoolarham910@gmail.com>
2026-08-14 22:02:38 +0530

    Configure GitHub Actions AWS OIDC

---
+++ github-oidc.tf
@@ -0,0 +1,55 @@
+# GitHub Actions OIDC Provider
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+# IAM Role for GitHub Actions
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-terraform-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:rasooldev910/terraform-aws-devops-project:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Permissions for Terraform
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Output the role ARN
+output "github_actions_role_arn" {
+  description = "IAM role ARN used by GitHub Actions"
+  value       = aws_iam_role.github_actions.arn
+}
