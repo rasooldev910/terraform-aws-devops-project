50d47317d1778b5de954b266b2fdebc5af621bf5
syed rasool <rasoolarham910@gmail.com>
2026-08-16 19:20:20 +0530

    Add S3 remote backend for shared state

--- providers.tf
+++ providers.tf
@@ -5,8 +5,15 @@ terraform {
       version = "~> 6.0"
     }
   }
-
   required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "rasooldev910-tfstate-635054837309"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
